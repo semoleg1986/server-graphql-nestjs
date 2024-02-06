4c0847f73eaacc706409f875d0e8ebc01fe9588e
semoleg1986 <semoleg1986@gmail.com>
2024-02-06 23:24:44 +0400

    feat: add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,31 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { PropertyResolver } from './graphql/resolvers/PropertyResolver';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+import { PropertyModule } from './property/property.module';
+import { Property } from './property/property.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://semoleg1986:asawakan@atlascluster.bixifap.mongodb.net/?retryWrites=true&w=majority',
+      useUnifiedTopology: true,
+      synchronize: true,
+      logging: true,
+      entities: [Property],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql',
     }),
+    PropertyModule,
+
   ],
   controllers: [],
-  providers: [PropertyResolver],
+  providers: [],
 })
 export class AppModule {}
